c1a39f45ff038a87211b76447e8191590815b6bd
Kentokugai <kentokugai@gmail.com>
2024-04-16 22:09:12 +0900

    ここまで

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'hero-pattern' : "url('/img/nikita-kachanovsky-OVbeSXRk_9E-unsplash.jpg')",
+        'hero-pattern2' : "url('/img/0_3.jpg')",
+      }
+    },
   },
   plugins: [],
 }
