ff77a1854fbcb2134594ab2bded459c4833eadb2
Kentokugai <kentokugai@gmail.com>
2024-04-16 23:48:06 +0900

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage:{
-        'hero-pattern' : "url('../img/nikita-kachanovsky-OVbeSXRk_9E-unsplash.jpg')",
-        'hero-pattern2' : "url('../img/0_3.jpg')",
-      }
+      
     },
   },
   plugins: [],
