c81a0ebd2976cc9b68c2343ef3ff2b91de316019
KentoKugai <145778437+KugaiKento@users.noreply.github.com>
2025-09-28 21:51:35 +0900

    astro-iconの削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,7 @@
+// @ts-check
 import { defineConfig } from "astro/config";
-import icon from "astro-icon";
 
-// ESM import が必要
-import heroicons from "@iconify-json/heroicons/icons.json";
-import mdi from "@iconify-json/mdi/icons.json";
-
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    icon({
-      collections: {
-        heroicons,
-        mdi,
-      },
-    }),
-  ],
+  integrations: [],
 });
